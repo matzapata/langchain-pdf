323ea01c6891e6a8ea9a18e82a08bb2b98b61dde
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-02-20 13:57:03 +0100

    multiple docs

---
+++ multiple-docs.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import json
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+documents = []
+
+# Create a List of Documents from all of our files in the ./docs folder
+for file in os.listdir("docs"):
+    if file.endswith(".pdf"):
+        pdf_path = "./docs/" + file
+        loader = PyPDFLoader(pdf_path)
+        documents.extend(loader.load())
+    elif file.endswith('.docx') or file.endswith('.doc'):
+        doc_path = "./docs/" + file
+        loader = Docx2txtLoader(doc_path)
+        documents.extend(loader.load())
+    elif file.endswith('.txt'):
+        text_path = "./docs/" + file
+        loader = TextLoader(text_path)
+        documents.extend(loader.load())
+
+# Split the documents into smaller chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+documents = text_splitter.split_documents(documents)
+
+# Convert the document chunks to embedding and save them to the vector store
+vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings(), persist_directory="./data")
+vectordb.persist()
+
+print("Vector store created")
+
+# create our Q&A chain
+pdf_qa = ConversationalRetrievalChain.from_llm(
+    ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo'),
+    retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
+    return_source_documents=True,
+    verbose=False
+)
+
+chat_history = []
+while True:
+    query = input(f"Prompt: ")
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
+        print('Exiting')
+        sys.exit()
+    if query == '':
+        continue
+    result = pdf_qa.invoke(
+        {"question": "only use provided context to answer:" + query, "chat_history": chat_history})
+    print(f"Answer: " + result["answer"])
+    chat_history.append((query, result["answer"]))
